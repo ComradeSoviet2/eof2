ae7a76bb2a8e87e4a25bdb12a3d90a41aee2f390
stalin123wtf <45238886+stalin123wtf@users.noreply.github.com>
2026-06-29 04:24:40 +0200

    start
    start

---
+++ common/defines/eof2_defines.lua
@@ -0,0 +1,19 @@
+NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 36
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 25			-- Days of client lag for decrease of gamespeed
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 25				-- Days of client lag for pause of gamespeed.
+NDefines.NGame.GAME_SPEED_SECONDS = { 0.35, 0.25, 0.2, 0.1, 0.0 }	-- game speeds for each level. Must be 5 entries with last one 0 for unbound
+
+NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 20			-- This much progress can be saved while not having a focus selected
+
+NDefines.NTechnology.MAX_SUBTECHS = 4				-- Max number of sub technologies a technology can have.
+
+
+
+
+
+
+NDefines.NBuildings.AIRBASE_CAPACITY_MULT = 100			-- Each level of airbase building multiplied by this, gives capacity (max operational value). Value is int. 1 for each airplane.
+NDefines.NBuildings.MAX_SHARED_SLOTS = 50			-- Max slots shared by factories
+
+NDefines.NMilitary.TRAINING_ATTRITION = 0		  	-- amount of extra attrition from being in training
+NDefines.NMilitary.TRAINING_EXPERIENCE_SCALE = 0
